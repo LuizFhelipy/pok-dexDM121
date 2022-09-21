75975c1640b0551e94e09b7fbc08f938f88e96c0
LuizFhelipy <fhelipy_rt@hotmail.com>
2022-09-21 10:01:56 -0300

    changed some things

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
 const generateHTML = pokemons => {
   return pokemons.reduce((accumulator, { name, id, types }) => {
     const elementTypes = types.map(typeInfo => typeInfo.type.name)
+    const pokemonId = ("000" + id).substr(-3)
 
     accumulator += `
       <li class="card ${elementTypes[0]}">
       <img class="card-image alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png"</img>
-        <h2 class="card-title">${id}. ${name}</h2>
-        <p class="card-subtitle">${elementTypes.join(" | ")}</p>
+        <h2 class="card-title">${name}</h2>
+        <h4 class="pokemon-id">#${pokemonId}</h4>
       </li>
     `
     return accumulator
