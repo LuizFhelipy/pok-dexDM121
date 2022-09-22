ad9d975cb28c9b0b1604b5091adfcc125d965c16
LuizFhelipy <fhelipy_rt@hotmail.com>
2022-09-21 22:37:51 -0300

    search bar

--- js/app.js
+++ js/app.js
@@ -24,6 +24,21 @@ const insertPokemonsIntoPage = pokemons => {
   ul.innerHTML = pokemons
 }
 
+function search_pokemon() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByTagName('li');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
+
 const pokemonPromises = generatePokemonPromises()
 
 Promise.all(pokemonPromises)
