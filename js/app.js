4a0989762d6d0f0d0713cc9b4904b632afece302
LuizFhelipy <fhelipy_rt@hotmail.com>
2022-09-24 15:23:11 -0300

    added sort

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
+// Variables
+let pokemonList = [];
+let toggles = {
+	alphabeticallyToggle: null,
+	numberToggle: null
+};
+
+// API
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
   (fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 )
+const pokemonPromises = generatePokemonPromises()	
 
+// Utils
 const generateHTML = pokemons => {
   return pokemons.reduce((accumulator, { name, id, types }) => {
     const elementTypes = types.map(typeInfo => typeInfo.type.name)
@@ -19,28 +29,58 @@ const generateHTML = pokemons => {
   }, "")
 }
 
+const sortItems = (list, sortType, field) => {
+	const conditionalSort = (a, b, ruleNumberOne, ruleNumberTwo) => {
+		if (a[field] < b[field]) return ruleNumberOne;
+		if (a[field] > b[field]) return ruleNumberTwo;
+		return 0;
+	};
+	
+	const sortedList = sortType
+		? list.sort((a, b) => conditionalSort(a, b, -1, 1))
+		: list.reverse((a, b) => conditionalSort(a, b, 1, -1));
+		
+	return sortedList;
+}
+
 const insertPokemonsIntoPage = pokemons => {
   const ul = document.querySelector('[data-js="pokedex"]')
   ul.innerHTML = pokemons
-}
+}	
 
-function search_pokemon() {
+// Events Handler
+function searchPokemon() {
   let input = document.getElementById('searchbar').value
-  input=input.toLowerCase();
-  let x = document.getElementsByTagName('li');
-    
-  for (i = 0; i < x.length; i++) { 
-      if (!x[i].innerHTML.toLowerCase().includes(input)) {
-          x[i].style.display="none";
-      }
-      else {
-          x[i].style.display="list-item";                 
-      }
-  }
+  let cardsElements = document.getElementsByTagName('li');
+  
+  Array.from(cardsElements).forEach(cardItem => {
+	 cardItem.style.display = !cardItem.innerHTML.toLowerCase().includes(input.toLowerCase())
+		  ? "none"
+		  : "list-item"; 
+  });
 }
 
-const pokemonPromises = generatePokemonPromises()
+function toggleAlphabeticallySort() {
+	toggles.alphabeticallyToggle = !toggles.alphabeticallyToggle;
+	const sortedList = toggles.alphabeticallyToggle ? sortItems(pokemonList, true, 'name') : sortItems(pokemonList, false, 'name');
+	
+	insertPokemonsIntoPage(generateHTML(sortedList));
+}
 
+function toggleIdSort() {
+	toggles.numberToggle = !toggles.numberToggle;
+	const sortedList = toggles.numberToggle ? sortItems(pokemonList, true, 'id') : sortItems(pokemonList, false, 'id');
+	
+	insertPokemonsIntoPage(generateHTML(sortedList));
+}
+
+// Events listener
+document.getElementById('sortAlpha').addEventListener('click', toggleAlphabeticallySort);
+document.getElementById('sortId').addEventListener('click', toggleIdSort);
+document.getElementById('searchbar').addEventListener('keyup', searchPokemon)
+
+// Initializer
 Promise.all(pokemonPromises)
+  .then(p => pokemonList = p)
   .then(generateHTML)
-  .then(insertPokemonsIntoPage)
+  .then(insertPokemonsIntoPage);
